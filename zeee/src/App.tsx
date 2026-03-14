232e01f8e4ed587da669600448e42c3808ae8e31
yoonyoungyang <youngonly0118@gmail.com>
2026-03-14 14:24:11 +0900

    tailwind css 적용 확인

--- zeee/src/App.tsx
+++ zeee/src/App.tsx
@@ -1,7 +1,15 @@
 import './index.css'
 
 function App() {
-  return <div className='h-24 w-full bg-red-100'>App</div>
+  return (
+    <>
+  <div className='h-48 w-full bg-white'>
+
+    <span className='' >React 컴포넌트 패턴 플레이그라운드</span>
+  </div>
+    <div className='w-full h-full bg-blue-50'>App</div>
+</>
+)
 }
 
 export default App
